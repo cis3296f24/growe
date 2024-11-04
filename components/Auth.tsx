0b382c7b2607c156271c9870405aa23c4f0f360f
bash <44552816+gutbash@users.noreply.github.com>
2024-11-03 22:34:46 -0500

    refactor auth screen steps

--- components/Auth.tsx
+++ components/Auth.tsx
@@ -2,9 +2,12 @@ import React, { useState } from 'react';
 import { View, TextInput, Button, Text, StyleSheet } from 'react-native';
 import { signUp, login, logout } from '../utils/authenticate';
 import { User } from 'firebase/auth';
+import Logo from '../assets/icons/logo.svg';
 
 export function Auth() {
+  const [step, setStep] = useState<'initial' | 'login-email' | 'login-password' | 'signup-email' | 'signup-username' | 'signup-password'>('initial');
   const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [user, setUser] = useState<User | null>(null);
 
@@ -21,33 +24,87 @@ export function Auth() {
   const handleLogout = async () => {
     await logout();
     setUser(null);
+    setStep('initial');
   };
 
   return (
     <View style={styles.container}>
-      <Text style={styles.welcomeText}>growe</Text>
-      <TextInput
-        placeholder="email"
-        value={email}
-        onChangeText={setEmail}
-        style={styles.input}
-      />
-      <TextInput
-        placeholder="password"
-        value={password}
-        onChangeText={setPassword}
-        secureTextEntry
-        style={styles.input}
-      />
+      <Logo width={50} height={50} style={styles.icon}/>
       {user ? (
         <View style={styles.userContainer}>
-          <Text style={styles.welcomeText}>Welcome, {user.email ?? 'User'}!</Text>
+          <Text style={styles.welcomeText}>welcome {username ?? 'user'}.</Text>
           <Button title="logout" onPress={handleLogout} />
         </View>
       ) : (
-        <View style={styles.buttonContainer}>
-          <Button title="sign up" onPress={handleSignUp} />
-          <Button title="login" onPress={handleLogin} />
+        <View>
+          {step === 'initial' && (
+            <View style={styles.buttonContainer}>
+              <Button title="sign up" onPress={() => setStep('signup-email')} />
+              <Button title="login" onPress={() => setStep('login-email')} />
+            </View>
+          )}
+          {step === 'login-email' && (
+            <View>
+              <TextInput
+                placeholder="Email or Username"
+                placeholderTextColor="gray"
+                value={email}
+                onChangeText={setEmail}
+                style={styles.input}
+              />
+              <Button title="next" onPress={() => setStep('login-password')} />
+            </View>
+          )}
+          {step === 'login-password' && (
+            <View>
+              <TextInput
+                placeholder="Password"
+                placeholderTextColor="gray"
+                value={password}
+                onChangeText={setPassword}
+                secureTextEntry
+                style={styles.input}
+              />
+              <Button title="login" onPress={handleLogin} />
+            </View>
+          )}
+          {step === 'signup-email' && (
+            <View>
+              <TextInput
+                placeholder="Email"
+                placeholderTextColor="gray"
+                value={email}
+                onChangeText={setEmail}
+                style={styles.input}
+              />
+              <Button title="next" onPress={() => setStep('signup-username')} />
+            </View>
+          )}
+          {step === 'signup-username' && (
+            <View>
+              <TextInput
+                placeholder="Username"
+                placeholderTextColor="gray"
+                value={username}
+                onChangeText={setUsername}
+                style={styles.input}
+              />
+              <Button title="next" onPress={() => setStep('signup-password')} />
+            </View>
+          )}
+          {step === 'signup-password' && (
+            <View>
+              <TextInput
+                placeholder="Password"
+                placeholderTextColor="gray"
+                value={password}
+                onChangeText={setPassword}
+                secureTextEntry
+                style={styles.input}
+              />
+              <Button title="sign up" onPress={handleSignUp} />
+            </View>
+          )}
         </View>
       )}
     </View>
@@ -55,17 +112,25 @@ export function Auth() {
 }
 
 const styles = StyleSheet.create({
+  icon: {
+    marginBottom: 12,
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
     padding: 16,
+    paddingBottom: 128,
   },
   input: {
     height: 40,
-    borderColor: 'gray',
+    minWidth: 200,
+    width: '100%',
+    borderColor: 'lightgray',
     borderWidth: 1,
     marginBottom: 12,
     paddingHorizontal: 8,
+    color: 'black',
+    borderRadius: 10,
   },
   userContainer: {
     alignItems: 'center',
