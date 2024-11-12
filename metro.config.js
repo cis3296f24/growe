c1548b6bf316dfc1d62a5d89dde18ca8674f0145
bash <44552816+gutbash@users.noreply.github.com>
2024-11-12 05:56:28 -0500

    add css to source exts

--- metro.config.js
+++ metro.config.js
@@ -17,7 +17,7 @@ module.exports = (() => {
   config.resolver = {
     ...resolver,
     assetExts: resolver.assetExts.filter((ext) => ext !== "svg"),
-    sourceExts: [...resolver.sourceExts, "svg"]
+    sourceExts: [...resolver.sourceExts, "svg", "css"]
   };
 
   return withNativeWind(config, { input: "./global.css" });
